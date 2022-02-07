5c4fa7413373dc48f470b5a2242042b5da540897
lalo09 <jose960808@gmail.com>
2022-02-07 17:41:10 -0600

    mejoras

--- applications/main/admin.py
+++ applications/main/admin.py
@@ -7,7 +7,7 @@ class RecetaAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at','updated_at','user',)
     
     list_display = (
-        'title','created_at','user'
+        'title','created_at','user','public','difficult'
     )
 
     def save_model(self,request,obj,form,change):
--- applications/main/models.py
+++ applications/main/models.py
@@ -18,11 +18,21 @@ class Categoria(models.Model):
         return self.name
 
 class Receta(models.Model):
+
+    DIFFICULT = (
+        ('0','Facil'),
+        ('1','Medio'),
+        ('2','Dificil')
+    )
+    
     title = models.CharField(max_length=100,verbose_name='Titulo')
+    descripcion = models.TextField(verbose_name='descripcion',null=True,blank=True)
     content = RichTextUploadingField(verbose_name="contenido")
+    time_preparation = models.CharField(max_length=100,verbose_name='Tiempo de preparacion',null=True,blank=True)
     image = models.ImageField(default='null',verbose_name="Imagen de presentacion",upload_to='recetas') 
     slug = models.CharField(unique=True,max_length=150,verbose_name='URL')
-    public = models.BooleanField(verbose_name="¿Visible?")
+    difficult = models.CharField("Dificultad",max_length=1,choices=DIFFICULT,null=True,blank=True)
+    public = models.BooleanField(verbose_name="¿Visible?",default=True)
     user = models.ForeignKey(User,verbose_name='Usuario',on_delete=models.CASCADE)
     category = models.ManyToManyField(Categoria,verbose_name='Categoria',null=True,blank=True) 
     created_at = models.DateTimeField(auto_now_add=True,verbose_name='Creado el')
